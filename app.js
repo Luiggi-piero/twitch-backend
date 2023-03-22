276e43bf09ebd3c4e2d718f870b62b9f56f55604
Luiggi Yantas Inocente <luiggiyantas@gmail.com>
2023-03-22 16:57:29 -0500

    configuración ffmpeg

--- app.js
+++ app.js
@@ -6,13 +6,29 @@ const config = {
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60,
+    ping_timeout: 60
   },
   http: {
     port: 8000,
-    allow_origin: "*",
+    mediaroot: './media', // lugar donde se almacenan los pedazos de transmision
+    allow_origin: '*'
   },
+  trans: {
+    ffmpeg: 'C:/ffmpeg-6.0-full_build/bin/ffmpeg.exe',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        dashKeep: true // to prevent dash file delete after end the stream
+      }
+    ]
+  }
 };
 
-var nms = new NodeMediaServer(config);
+var nms = new NodeMediaServer(config)
 nms.run();
+// C:\ffmpeg-6.0-full_build\bin
